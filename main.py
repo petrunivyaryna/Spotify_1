1ac31979eb1d09978550cd83afd1bec04a43620b
yarkapetruniv <petruniv.pn@ucu.edu.ua>
2023-02-25 13:35:43 +0200

    changes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """This module is created to parse the json from Spotify.
 
 This module works with jsin file which is obtained using the Spotify API.
-T run this module the user must enter the name of the artist.
+To run this module the user must enter the name of the artist.
 Using this name the module can search for artist name, artist ID,
 the most popular song of the artist and available markets for this
 song. Then the user can choose what the module will show him.
