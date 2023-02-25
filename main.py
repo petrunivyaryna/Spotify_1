1028c70349ba314cb5a33930b56482af6fcbc641
yarkapetruniv <petruniv.pn@ucu.edu.ua>
2023-02-25 13:51:25 +0200

    obtained token and authorization header

--- main.py
+++ main.py
@@ -8,8 +8,47 @@ song. Then the user can choose what the module will show him.
 
 link to github: https://github.com/yarkapetruniv/Spotify_1.git
 """
+import os
+import base64
+import json
+from requests import post
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
+
+def get_token():
+    """
+    This function should access token, which is used to
+    make API calls on behalf the user or application.
+    """
+    authorization_str = client_id + ":" + client_secret
+    authorization_bytes = authorization_str.encode('utf-8')
+    authorization_base64 = str(base64.b64encode(authorization_bytes), 'utf-8')
+
+    url = "https://accounts.spotify.com/api/token"
+    headers = {
+        "Authorization": "Basic " + authorization_base64,
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    data = {"grant_type": "client_credentials"}
+    result = post(url, headers = headers, data = data, timeout=1.5)
+    json_result = json.loads(result.content)
+    token = json_result["access_token"]
+    return token
 
 
+def get_authorization_header():
+    """
+    This function is created to get authorization header
+    everytime you have to find some new information using token.
+    """
+    return {"Authorization": "Bearer " + get_token()}
+
+
+my_token = get_token()
 
 
 
