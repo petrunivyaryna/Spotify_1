6a7c64ab153021ce975d155f1b8a9614ed2fb3f2
yarkapetruniv <petruniv.pn@ucu.edu.ua>
2023-02-27 16:49:35 +0200

    added functions

--- main.py
+++ main.py
@@ -114,6 +114,46 @@ def get_available_markets(song, artist_id):
     return None
 
 
+def get_all_albums(artist_id):
+    """
+    This function should return all albums of the
+    appropriate artist using artist's ID. And also the id
+    of every album.
+    >>> get_all_albums('5YGY8feqx7naU7z4HrwZM6')[:3]
+    [('ATTENTION: MILEY LIVE', '5G5s00CN4Kmxz340ED2WL2'), \
+('Plastic Hearts', '5BRhg6NSEZOj0BR6Iz56fR'), ('Plastic Hearts', \
+'0BCjAmbM8ryCM9gxy5yW7h')]
+    """
+    url = f"https://api.spotify.com/v1/artists/{artist_id}/albums"
+    headers = get_authorization_header()
+    result = get(url, headers = headers, timeout = 1.5)
+    json_result = json.loads(result.content)['items']
+    albums_of_artist = []
+    ids = []
+    for album in json_result:
+        if album['name'] not in albums_of_artist:
+            albums_of_artist.append(album['name'])
+            ids.append(album['id'])
+    albums = zip(albums_of_artist, ids)
+    return list(albums)
+
+
+def  get_song_from_album(album_id):
+    """
+    >>> get_song_from_album('0BCjAmbM8ryCM9gxy5yW7h')[:5]
+    ['WTF Do I Know', 'Plastic Hearts', 'Angels Like You', \
+'Prisoner (feat. Dua Lipa)', 'Gimme What I Want']
+    """
+    url = f"https://api.spotify.com/v1/albums/{album_id}/tracks"
+    headers = get_authorization_header()
+    result = get(url, headers = headers, timeout = 1.5)
+    json_result = json.loads(result.content)['items']
+    tracks = []
+    for track in json_result:
+        tracks.append(track['name'])
+    return tracks
+
+
 def print_information(name_artist: str):
     """
     This function is created to display the data that the user wants to view.
@@ -122,24 +162,47 @@ def print_information(name_artist: str):
     artist_id = search_for_artist(name_artist)[1]
     most_popular_song = get_song(artist_id)
     available_markets = get_available_markets(most_popular_song, artist_id)
+    albums = get_all_albums(artist_id)
     dictionary = {'1': f"Artist's name: {artist_name}", '2': f"{artist_name}'s ID: {artist_id}",'3':
-                 f"{artist_name}'s most popular song: {most_popular_song}", '4': available_markets}
+                 f"{artist_name}'s most popular song: {most_popular_song}", '4': available_markets,
+                 '5': albums}
 
     print(f"The information you can see:\
           \n1. Artist's name\n2. {artist_name}'s ID\
           \n3. The most popular {artist_name}'s song\
-          \n4. Available markets for {artist_name}'s most popular song\n")
+          \n4. Available markets for {artist_name}'s most popular song\
+          \n5. {artist_name}'s albums\n")
     to_print = input("Write down the number (if you want to choose several numbers, \
 just write them using comma): ")
     for key in list(dictionary.keys()):
         if key in to_print:
-            if key != '4':
-                print(dictionary[key] + '\n')
-            else:
+            if key == '4':
                 num_of_markets = input(f"There are {len(available_markets)} \
 available markets for {artist_name}'s most popular song.\
                                        \nHow many of them you wanna see? (write down a number): ")
                 print(f"Available markets: {dictionary[key][:int(num_of_markets)]}")
+            elif key == '5':
+                albumss = {}
+                num_of_albums = input(f"\nThere are {len(albums)} albums written by {artist_name}.\
+                                       \nHow many of them you wanna see? (write down a number): ")
+                print(f"{artist_name}'s albums:")
+                for ind, album in enumerate(albums[:int(num_of_albums)]):
+                    print(f"{ind + 1}. {album[0]}")
+                    albumss[str(ind + 1)] = album
+                yes_no = input("\nDo you want to see which songs are on a particular album? \
+(write down a number: 1 - yes, 2 - no): ")
+                if yes_no == '1':
+                    num_albums = input("Choose the album (if you want to choose several albums, \
+just write them using comma): ")
+                    for key in list(albumss.keys()):
+                        if key in num_albums:
+                            print(f"{albumss[key][0]}'s songs:")
+                            songs = get_song_from_album(albumss[key][1])
+                            for ind, song in enumerate(songs):
+                                print(f"{ind + 1}. {song}")
+                            print("\n")
+            else:
+                print(dictionary[key] + '\n')
 
 name_of_artist = input("Enter the name of the artist: ")
 print_information(name_of_artist)
